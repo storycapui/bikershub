99fdbee954058fdc8170d1156778836b651e4cd1
surya prakash <155574828+storycapui@users.noreply.github.com>
2024-07-10 16:42:50 +0530

    push

--- frontend/src/components/Assets/best_sellers.js
+++ frontend/src/components/Assets/best_sellers.js
@@ -5,6 +5,7 @@ import image4 from "./image4.jpg";
 import image5 from "./image5.jpg";
 import { IoLogoWhatsapp } from "react-icons/io";
 import { FaSquareInstagram } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
 
 let best_sellers = [
   {
@@ -12,7 +13,7 @@ let best_sellers = [
     name: "Acrophovic Silencer",
     category: "Accessories",
     image: image1,
-    new_price: "₹ 8500.0/-",
+    new_price: "₹8500.0/-",
     old_price: "12000.5/-",
     sizes : ["S", "L", "M"],
     colors: ["#000000", "#ffffff", "#zzzzzz"],
@@ -20,13 +21,14 @@ let best_sellers = [
     discount : "25%",
     whatsapp : <IoLogoWhatsapp />,
     instagram : <FaSquareInstagram  />,
+    youtube : <FaYoutube />,
   },
   {
     id: 2,
     name: "KYT NCR Hemlets",
     category: "Helmets",
     image: image2,
-    new_price: "₹ 12500.0/-",
+    new_price: "₹12500.0/-",
     old_price: "17000.5/-",
     sizes : ["S", "L", "M"],
     colors: ["#000000", "#ffffff", "#zzzzzz"],
@@ -34,13 +36,14 @@ let best_sellers = [
     discount : "25%",
     whatsapp : <IoLogoWhatsapp />,
     instagram : <FaSquareInstagram />,
+    youtube : <FaYoutube />,
   },
   {
     id: 3,
     name: "Rynox Riding Jackets",
     category: "Jackets",
     image: image3,
-    new_price: "₹ 18500.0/-",
+    new_price: "₹18500.0/-",
     old_price: "22000.5/-",
     sizes : ["S", "L", "M"],
     colors: ["#000000", "#ffffff", "#zzzzzz"],
@@ -48,13 +51,14 @@ let best_sellers = [
     discount : "25%",
     whatsapp : <IoLogoWhatsapp />,
     instagram : <FaSquareInstagram />,
+    youtube : <FaYoutube />,
   },
   {
     id: 4,
     name: "Rynox Safety Guards",
     category: "Gears",
     image: image4,
-    new_price: "₹ 22500.0/-",
+    new_price: "₹22500.0/-",
     old_price: "27000.5/-",
     sizes : ["S", "L", "M"],
     colors: ["#000000", "#ffffff", "#zzzzzz"],
@@ -62,13 +66,14 @@ let best_sellers = [
     discount : "25%",
     whatsapp : <IoLogoWhatsapp />,
     instagram : <FaSquareInstagram />,
+    youtube : <FaYoutube />,
   },
   {
     id: 5,
     name: "Mobile Phone Mount",
     category: "accessories",
     image: image5,
-    new_price: "₹ 5000.0/-",
+    new_price: "₹5000.0/-",
     old_price: "6000.5/-",
     sizes : ["S", "L", "M"],
     colors: ["#000000", "#ffffff", "#zzzzzz"],
@@ -76,6 +81,7 @@ let best_sellers = [
     discount : "25%",
     whatsapp : <IoLogoWhatsapp />,
     instagram : <FaSquareInstagram />,
+    youtube : <FaYoutube />,
   },
 ];
 
--- frontend/src/components/item/item.js
+++ frontend/src/components/item/item.js
@@ -6,10 +6,10 @@ const Item = (props) => {
     <div className='item'> 
     <div className='discount'><p>{props.discount}</p></div>
     <img className='prod_image' src={props.image} alt="" />
-    <div className='prod_name'><h3>{props.name}</h3></div>
+    <div className='prod_name'><h4>{props.name}</h4></div>
     <div className='item-prices'>
         <div className='item-price-new'>
-            {props.new_price}
+            <p>{props.new_price}</p>
         </div>
         <div className='item-price-old'>
             {props.old_price}
@@ -20,11 +20,12 @@ const Item = (props) => {
     <div className='add_to_cart'> <button> ADD TO CART </button> </div>
     </div>
     <div className='stock_avail'>
-      <h3>{props.stock}</h3>
+      <h5>{props.stock}</h5>
     </div>
     <div className='social_media'>
       <section className='whatsapp_icon'>{props.whatsapp}</section>
        <section className='instagram_icon'>{props.instagram}</section> 
+       <section className='youtube_icon'>{props.youtube}</section>
     </div>
     </div>
   )
