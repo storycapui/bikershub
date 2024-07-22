41ebb8a6b2d363c4dfc6ced579efa58fc417625d
surya prakash <155574828+storycapui@users.noreply.github.com>
2024-07-22 21:10:57 +0530

    commit changes

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './Footer.css';
 import bikershub from '../Assets/bikershub.jpg'
+import { FaInstagram } from "react-icons/fa6";
+import { FaTelegramPlane } from "react-icons/fa";
+import { CiTwitter } from "react-icons/ci";
+import { FaFacebookF } from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
+
+
+
 
 const Footer = () => {
     return (
@@ -19,9 +27,24 @@ const Footer = () => {
         </div>
         <div className='footer_links'>
             <div className='logo_social_icons'>
-                
+            <br/>
+                <img src={bikershub} alt="" className='footer_logo' />
+                <br/>
+                <h3>Follow us</h3>
+                <br/>
+                <span><FaInstagram /> &nbsp; <FaTelegramPlane /> &nbsp;<CiTwitter />&nbsp; <FaFacebookF /> </span>
             </div>
-            <div className='Product_links'></div>
+            <div className='Product_links'>
+                <h2>Product <hr/></h2>
+                <ul className='useful_links'>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/helmets">Helmets</NavLink>
+                    <NavLink to="/accessories">Accessories</NavLink>
+                    <NavLink to="/jackets">Jackets</NavLink>
+                    <NavLink to="/spares">Spares</NavLink>
+                    <NavLink to="/womens">Womens</NavLink>
+                </ul>
+            </div>
             <div className='footer_useful_links'></div>
             <div className='company_links'></div>
             <div className='address_links'></div>
